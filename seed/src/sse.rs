e8a40922c4172ae836b3d2feff0cab712e2ceb7e
Alexis Sellier <self@cloudhead.io>
2020-10-22 14:34:09 +0200

    Hook up richer events

--- seed/src/sse.rs
+++ seed/src/sse.rs
@@ -13,17 +13,28 @@ pub async fn to_sse(event: seed::Event) -> Option<Result<impl sse::ServerSentEve
             "type": "listening",
             "addr": addr.to_string()
         })))),
-        seed::Event::PeerConnected(id) => Some(Ok(sse::json(json!({
+        seed::Event::PeerConnected { peer_id, urn, name } => Some(Ok(sse::json(json!({
             "type": "peer-connected",
-            "id": id.to_string()
+            "id": peer_id.to_string(),
+            "urn": urn.map(|u| u.to_string()),
+            "name": name.map(|u| u.to_string()),
         })))),
         seed::Event::PeerDisconnected(id) => Some(Ok(sse::json(json!({
             "type": "peer-disconnected",
             "id": id.to_string()
         })))),
-        seed::Event::ProjectTracked(urn, _) => Some(Ok(sse::json(json!({
+        seed::Event::ProjectTracked {
+            urn,
+            name,
+            description,
+            maintainers,
+            ..
+        } => Some(Ok(sse::json(json!({
             "type": "project-tracked",
             "urn": urn.to_string(),
+            "name": name,
+            "description": description,
+            "maintainers": maintainers
         })))),
     }
 }
