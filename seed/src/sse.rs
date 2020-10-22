eb4c7ce19843c725d50dabcef1175c9752146318
Alexis Sellier <self@cloudhead.io>
2020-10-22 11:16:39 +0200

    Use "type" for the event variant

--- seed/src/sse.rs
+++ seed/src/sse.rs
@@ -10,19 +10,19 @@ use serde_json::json;
 pub async fn to_sse(event: seed::Event) -> Option<Result<impl sse::ServerSentEvent, Infallible>> {
     match event {
         seed::Event::Listening(addr) => Some(Ok(sse::json(json!({
-            "tag": "listening",
+            "type": "listening",
             "addr": addr.to_string()
         })))),
         seed::Event::PeerConnected(id) => Some(Ok(sse::json(json!({
-            "tag": "peer-connected",
+            "type": "peer-connected",
             "id": id.to_string()
         })))),
         seed::Event::PeerDisconnected(id) => Some(Ok(sse::json(json!({
-            "tag": "peer-disconnected",
+            "type": "peer-disconnected",
             "id": id.to_string()
         })))),
         seed::Event::ProjectTracked(urn, _) => Some(Ok(sse::json(json!({
-            "tag": "project-tracked",
+            "type": "project-tracked",
             "urn": urn.to_string(),
         })))),
     }
