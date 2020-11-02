e831949d767ddd1a293382720aabd45cd6459e65
Alexis Sellier <self@cloudhead.io>
2020-11-02 12:44:08 +0100

    seed: Format seed-id correctly and make copyable

--- seed/ui/src/Components/Header.svelte
+++ seed/ui/src/Components/Header.svelte
@@ -1,5 +1,10 @@
 <script>
+  import Copyable from "./Copyable.svelte";
   export let seed = null;
+
+  $: seedId = seed.publicAddr
+    ? `${seed.peerId  }@${  seed.publicAddr}`
+    : seed.peerId;
 </script>
 
 <style>
@@ -26,11 +31,19 @@
     color: var(--color-foreground-level-6);
     line-height: 120%;
   }
+
+  .seed-id {
+    font-size: 70%;
+  }
 </style>
 
 <div class="meta">
   <h2>{seed.name}</h2>
-  <p class="address typo-text-small-mono"><span>{seed.peerId}</span></p>
+  <p class="address">
+    <Copyable showIcon styleContent={false} copyContent={seedId}>
+      <p class="typo-text-small-mono seed-id">{seedId}</p>
+    </Copyable>
+  </p>
   {#if seed.description}
     <p class="desc">
       {@html seed.description}
