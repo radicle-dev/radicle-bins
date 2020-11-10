691c9cd20065671bd407452951309611b4b8d19b
Kim Altintop <kim.altintop@gmail.com>
2020-11-10 10:44:00 +0100

    Set up CI

--- keyutil/src/main.rs
+++ keyutil/src/main.rs
@@ -1,10 +1,24 @@
+// This file is part of radicle-bins
+// <https://github.com/radicle-dev/radicle-bins>
+//
+// Copyright (C) 2019-2020 The Radicle Team <dev@radicle.xyz>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 or
+// later as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use std::{fs, fs::File, io::prelude::*, os::unix::fs::PermissionsExt, path::PathBuf};
+
 use argh::FromArgs;
 use librad::keys::SecretKey;
-use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
-use std::os::unix::fs::PermissionsExt;
-use std::path::PathBuf;
 
 #[derive(FromArgs)]
 /// Utility for generating a private key file for Radicle Seed.
