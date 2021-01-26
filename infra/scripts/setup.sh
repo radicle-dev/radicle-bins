1833880f6f33a93997107b0ea3975fede68178e8
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-01-26 13:21:16 +0000

    Update setup script
    Use version for grabbing the artifacts from a tag versioned bin.
    Add common curl options.
    
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- infra/scripts/setup.sh
+++ infra/scripts/setup.sh
@@ -2,17 +2,17 @@
 set -eou pipefail
 
 ROOT=/state
-ARTIFACT_URL="https://storage.googleapis.com/builds.radicle.xyz/radicle-bins/master/${COMMIT}"
+ARTIFACT_URL="https://storage.googleapis.com/builds.radicle.xyz/radicle-bins/v{VERSION}/dist"
 
 # Download the binaries
-curl ARTIFACT_URL/dist/radicle-keyutil -o /usr/bin/radicle-keyutil
-curl ARTIFACT_URL/dist/radicle-seed-node -o /usr/bin/radicle-seed-node
+curl -sfLS ARTIFACT_URL/radicle-keyutil -o /usr/bin/radicle-keyutil
+curl -sfLS ARTIFACT_URL/radicle-seed-node -o /usr/bin/radicle-seed-node
 
 # Download the systemd service unit
-curl ARTIFACT_URL/dist/seed.service -o /etc/systemd/system/seed.service
+curl -sfLS ARTIFACT_URL/seed.service -o /etc/systemd/system/seed.service
 
 # Download and unpack the assets
-curl ARTIFACT_URL/dist/assets.tar.gz -o $ROOT/assets.tar.gz
+curl -sfLS ARTIFACT_URL/assets.tar.gz -o $ROOT/assets.tar.gz
 tar -zxvf $ROOT/assets.tar.gz
 
 systemctl enable seed
