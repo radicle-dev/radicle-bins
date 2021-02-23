6124cfeaba9561372c85e66f79263524a18f630b
Fintan Halpenny <fintan.halpenny@gmail.com>
2021-02-23 16:10:17 +0000

    WIP: Updating the seed frontend to use net/next
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- seed/src/frontend.rs
+++ seed/src/frontend.rs
@@ -27,15 +27,13 @@ use std::{
 use futures::{channel::mpsc as chan, StreamExt as _};
 use serde::Serialize;
 use tokio::sync::Mutex;
+use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::Filter as _;
 
 use avatar::Avatar;
 use librad::{git::Urn, peer::PeerId};
 use radicle_avatar as avatar;
 use radicle_seed as seed;
-
-/// Maximum number of disconnected peers to keep around in the state.
-const MAX_DISCONNECTED_PEERS: usize = 32;
 
 #[derive(Debug, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
@@ -107,78 +105,6 @@ impl State {
             Entry::Occupied(_) => {},
         }
     }
-
-    fn peer_connected(&mut self, peer_id: PeerId, urn: Option<Urn>, name: Option<String>) {
-        match self.peers.entry(peer_id) {
-            Entry::Vacant(entry) => {
-                let user = urn.map(|u| User::from(u).with_name(name));
-                let peer = Peer {
-                    peer_id,
-                    user,
-                    state: PeerState::new(),
-                };
-                entry.insert(peer.clone());
-
-                self.broadcast(Event::PeerConnected(peer));
-            },
-            Entry::Occupied(mut entry) => {
-                let peer = entry.get_mut();
-
-                match &mut peer.state {
-                    PeerState::Connected { connections } => {
-                        *connections += 1;
-                    },
-                    PeerState::Disconnected { .. } => {
-                        peer.state = PeerState::new();
-                    },
-                }
-            },
-        }
-    }
-
-    fn peer_disconnected(&mut self, peer_id: PeerId) {
-        if let Entry::Occupied(mut entry) = self.peers.entry(peer_id) {
-            let peer = entry.get_mut();
-
-            match &mut peer.state {
-                PeerState::Connected { connections } => {
-                    if *connections > 1 {
-                        *connections -= 1;
-                    } else {
-                        peer.state = PeerState::Disconnected {
-                            since: time::SystemTime::now(),
-                        };
-                        self.broadcast(Event::PeerDisconnected { peer_id });
-                    }
-                },
-                PeerState::Disconnected { .. } => {},
-            }
-        }
-
-        // Remove oldest disconnected peer if necessary.
-        if self
-            .peers
-            .values()
-            .filter(|p| matches!(p.state, PeerState::Disconnected {..}))
-            .count()
-            > MAX_DISCONNECTED_PEERS
-        {
-            if let Some((oldest, _)) = self
-                .peers
-                .iter()
-                .flat_map(|(id, p)| {
-                    if let PeerState::Disconnected { since } = p.state {
-                        Some((*id, since))
-                    } else {
-                        None
-                    }
-                })
-                .min_by(|(_, a), (_, b)| a.cmp(b))
-            {
-                self.peers.remove(&oldest);
-            }
-        }
-    }
 }
 
 #[derive(Debug, Serialize, Clone)]
@@ -199,14 +125,14 @@ impl Peer {
 #[serde(rename_all = "camelCase", tag = "type")]
 pub enum PeerState {
     #[serde(rename_all = "camelCase")]
-    Connected { connections: usize },
+    Connected,
     #[serde(rename_all = "camelCase")]
     Disconnected { since: time::SystemTime },
 }
 
 impl PeerState {
     fn new() -> Self {
-        Self::Connected { connections: 1 }
+        Self::Connected
     }
 }
 
@@ -269,12 +195,8 @@ async fn fanout(state: Arc<Mutex<State>>, mut events: chan::Receiver<seed::Event
             seed::Event::ProjectTracked(proj, _) => {
                 state.project_tracked(proj);
             },
-            seed::Event::PeerConnected { peer_id, urn, name } => {
-                state.peer_connected(peer_id, urn, name);
-            },
-            seed::Event::PeerDisconnected(peer_id) => {
-                state.peer_disconnected(peer_id);
-            },
+            // FIXME: potential to report the status of the seed
+            seed::Event::Disconnected => {},
             seed::Event::Listening(_) => {},
         };
     }
@@ -308,6 +230,7 @@ pub async fn run<A: Into<net::SocketAddr>>(
 
     tokio::task::spawn(fanout(state.clone(), events));
 
+    let handle = Arc::new(Mutex::new(handle));
     let projects = warp::path("projects")
         .map({
             let state = state.clone();
@@ -317,8 +240,7 @@ pub async fn run<A: Into<net::SocketAddr>>(
 
     let peers = warp::path("peers")
         .map({
-            let state = state.clone();
-            move || state.clone()
+            move || handle.clone()
         })
         .and_then(peers_handler);
 
@@ -332,13 +254,11 @@ pub async fn run<A: Into<net::SocketAddr>>(
         .await;
 }
 
-async fn peers_handler(state: Arc<Mutex<State>>) -> Result<impl warp::Reply, warp::Rejection> {
-    let state = state.lock().await;
-    let peers = state.peers.clone();
+async fn peers_handler(handle: Arc<Mutex<seed::NodeHandle>>) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut handle = handle.lock().await;
+    let peers = handle.get_peers().await.expect("failed to get peer list");
 
-    Ok(warp::reply::json(
-        &peers.values().cloned().collect::<Vec<_>>(),
-    ))
+    Ok(warp::reply::json(&peers))
 }
 
 async fn projects_handler(state: Arc<Mutex<State>>) -> Result<impl warp::Reply, warp::Rejection> {
@@ -374,6 +294,6 @@ async fn events_handler(state: Arc<Mutex<State>>) -> Result<impl warp::Reply, wa
     };
 
     Ok(warp::sse::reply(warp::sse::keep_alive().stream(
-        receiver.map(|e| Ok::<_, Infallible>(warp::sse::json(e))),
+        UnboundedReceiverStream::new(receiver).map(|e| Ok::<_, Infallible>(warp::sse::json(e))),
     )))
 }
