db95b873c804c20250bf695707b5ac7b07a81bfb
Julien Donck <hello@juliendonck.com>
2021-04-22 15:58:03 +0200

    format dates

--- seed/ui/src/Components/Peer.svelte
+++ seed/ui/src/Components/Peer.svelte
@@ -61,7 +61,12 @@
           <p class="status-indicator">online</p>
         {:else}
           <p class="time typo-text-small">
-            {peer.state.since.toLocaleString()}
+            {peer.state.since.toLocaleDateString('default', {
+              month: 'long',
+              day: '2-digit',
+              hour: 'numeric',
+              minute: 'numeric',
+            })}
           </p>
         {/if}
       </div>
