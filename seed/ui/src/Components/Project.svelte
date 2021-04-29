5455cd05dc73270d10727e33b0e3dc8924a57f51
Julien Donck <hello@juliendonck.com>
2021-04-29 17:40:10 +0200

    move avatar
    Signed-off-by: Julien Donck <hello@juliendonck.com>

--- seed/ui/src/Components/Project.svelte
+++ seed/ui/src/Components/Project.svelte
@@ -4,7 +4,6 @@
   import Icon from "./Icon";
   import Copyable from "./Copyable.svelte";
   import Avatar from "./Avatar.svelte";
-  import Badge from "./Badge.svelte";
 
   export let project = null;
 </script>
@@ -27,10 +26,10 @@
     margin: 1rem 0 0rem;
   }
 
-  .maintainer {
+  .title {
     display: flex;
-    margin-top: 1.5rem;
     align-items: center;
+    margin-bottom: 0.75rem;
   }
 
   .radicle-id {
@@ -38,7 +37,7 @@
   }
 
   h2 {
-    margin-bottom: 0.75rem;
+    margin-right: 0.75rem;
   }
 
   a.button {
@@ -62,7 +61,10 @@
 </style>
 
 <div class="container">
-  <h2>{project.name}</h2>
+  <div class="title">
+    <h2>{project.name}</h2>
+    <Avatar avatar={project.maintainers[0].avatar} />
+  </div>
   <div class="radicle-id">
     <Icon.At style="margin-right: 0.25rem;" />
     <Copyable showIcon={true} styleContent={false} copyContent={project.urn}>
@@ -72,15 +74,6 @@
     </Copyable>
   </div>
   <p class="typo-text desc">{project.description}</p>
-  <div class="maintainer">
-    {#each project.maintainers as maintainer}
-      <Avatar avatar={maintainer.avatar} />
-      {#if maintainer.name}
-        <p class="typo-text-bold">{maintainer.name}</p>
-        <Badge caption="maintainer" style="margin-left: .5rem;" />
-      {/if}
-    {/each}
-  </div>
   <a class="button" href={`radicle://link/v0/${project.urn}`}>
     <Icon.ArrowBoxUpRight style="margin-right: 0.8rem;" />View in Upstream
   </a>
